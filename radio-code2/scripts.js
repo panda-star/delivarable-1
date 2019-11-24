1247ae538dba9eb04f2e64aa15dd04680b114161
dean <deanreagon13@gmail.com>
2019-11-24 22:25:09 +0200

    uneccessary file removed

--- radio-code2/scripts.js
+++
@@ -1,8 +0,0 @@
-// var radionbtns = document.querySelectorAll('.radio-item');
-
-// for( i=0; i<radionbtns.length; i++ ) {
-//     radionbtns[i].addEventListener('change',function(){
-//         document.querySelector('#colourOutput').value = this.value;
-//         this.border= 10px solid red; 
-//     });
-// }
