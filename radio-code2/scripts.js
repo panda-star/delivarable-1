994e580f45096e33ec92e9ebf54578007498652e
dean <deanreagon13@gmail.com>
2019-11-24 12:37:18 +0200

    added css styling demo

---
+++ radio-code2/scripts.js
@@ -0,0 +1,8 @@
+// var radionbtns = document.querySelectorAll('.radio-item');
+
+// for( i=0; i<radionbtns.length; i++ ) {
+//     radionbtns[i].addEventListener('change',function(){
+//         document.querySelector('#colourOutput').value = this.value;
+//         this.border= 10px solid red; 
+//     });
+// }
