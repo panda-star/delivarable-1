843a266021716ba387bd4f279559cd9d64f96b89
dean <deanreagon13@gmail.com>
2019-11-28 21:10:51 +0200

    added functionality to increment and decrement btns

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 
 var btnarray = document.getElementsByClassName("colour-btn");
+var incbtn = document.getElementById("increment");
+var decbtn = document.getElementById("decrement");
+var amount = 0;
 
 for( i=0; i<btnarray.length; i++ ) {
   btnarray[i].style.backgroundColor = btnarray[i].id ;
@@ -7,4 +10,18 @@ for( i=0; i<btnarray.length; i++ ) {
     document.querySelector("#Output1") .value = this.id;
     document.querySelector("#Output2") .value = this.id; 
   }
+}
+
+incbtn.onclick = function(event) {
+  // amount.value = "1";
+  amount = amount + 1;
+  document.querySelector("#Output3") .value = amount;
+}
+
+decbtn.onclick = function(event) {
+  if (amount > 0) {
+    amount = amount - 1;
+    document.querySelector("#Output3") .value = amount;
+  } 
+  
 }
