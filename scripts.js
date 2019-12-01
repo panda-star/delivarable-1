5a64c261b6166bd82eecf9dcc3c0e3901a86ae0e
dean <deanreagon13@gmail.com>
2019-12-01 17:46:11 +0200

    made add btn toggle add or checkout modal depending on it's innerHTML

--- scripts.js
+++ scripts.js
@@ -5,7 +5,20 @@ var decbtn = document.getElementById("decrement");
 var amount = 0;
 var agreebtn = document.getElementById("agree");
 var cancelbtn = document.getElementById("cancel");
-var checkoutbtn;
+var add = document.getElementById("add");
+
+add.onclick = function(event) {
+  if(add.innerHTML === "Add to Cart" ){
+    $("#addModal").modal('toggle');
+    console.log("test2");
+  }
+  else {
+    console.log("test3");
+    console.log(add.innerHTML);
+    $("#checkoutModal").modal('toggle');
+  }
+}
+ 
 
 for( i=0; i<btnarray.length; i++ ) {
   btnarray[i].style.backgroundColor = btnarray[i].id ;
@@ -36,12 +49,8 @@ cancelbtn.onclick = function(event) {
 agreebtn.onclick = function(event) {
   if (amount > 0) {
     document.querySelector("#Output4") .value = amount;
-    checkoutbtn = document.getElementById("add/checkout");
-    checkoutbtn.innerHTML = "checkout now";
-    // checkoutbtn.dataTarget = "#checkoutModal";
-    // checkoutbtn.onclick(
-    //   document.getElementById("checkourModal").style.display = "block" 
-    // );
+    // 
+    add.innerHTML = "checkout now";
     detailsCreate(amount,document.querySelector("#Output1") .value);
     amount = 0;
     document.querySelector("#Output3") .value = amount;
